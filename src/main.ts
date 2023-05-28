f3a9f7c9ea26bb761bc54c2a109d7852563bab60
Tae kh4 <taeanstae248@gmail.com>
2023-05-28 14:42:08 +0700

    ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+  const clientOrigin =
+    configService.get<string>('CLIENT_ORIGIN') || 'http://localhost:3000';
+  app.use(helmet());
+  app.enableCors({
+    origin: clientOrigin,
+    credentials: true,
+  });
+  await app.listen(port);
 }
 bootstrap();
